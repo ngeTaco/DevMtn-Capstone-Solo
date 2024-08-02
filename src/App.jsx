c7bb77ddf87c4e27010e0e43cd9f0a762d98c878
ngeTaco <thealdo.531@gmail.com>
2024-08-01 21:32:24 -0600

    basic skeleton on app home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
 import './App.css'
 
 
+function App() {
 
+  return (
+    <div>
+
+      <header className="App-Header">
+        <article>
+          <h1>Storage Tracker</h1>
+          <p>Breadcrumbs / Placeholder</p>
+          <div>
+            <button>Add Unit</button>
+            <button>Edit Mode</button>
+          </div>
+        </article>
+      </header>
+
+
+      <article>
+        <div className="Location-Tile">
+          <img src="https://picsum.photos/seed/picsum/50"></img>
+          <h2>Location01</h2>
+          <button>Go To Location</button>
+          <button>Edit</button>
+          <button>Save</button>
+        </div>
+
+        <div className="Location-Tile">
+          <img src="https://picsum.photos/seed/picsum/50"></img>
+          <h2>Location02</h2>
+          <button>Go To Location</button>
+          <button>Edit</button>
+          <button>Save</button>
+        </div>
+
+        <div className="Location-Tile">
+          <img src="https://picsum.photos/seed/picsum/50"></img>
+          <h2>Location03</h2>
+          <button>Go To Location</button>
+          <button>Edit</button>
+          <button>Save</button>
+        </div>
+      </article>
+
+    </div>
+  )
+}
 
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -15,3 +15,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+
+// https://ed.devmountain.com/materials/ogptlh2/lectures/wb-react-router/
+// router provider, create browserrouter from router-dom
+// make a router with cbr
+// instead of line 15 do router provier
+
+//outlet from router-dom into all of the pages
